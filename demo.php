32cb51b5d116432e068320c1b33ed217a6d71208
knight0zh <knight0zh@hotmail.com>
2019-09-18 17:26:45 +0800

    init

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: knight0zh
+ * Date: 2019/9/18
+ * Time: 下午5:25
+ */
+require_once './vendor/autoload.php';
+use Xhprof\Sdk\Xhprof;
+Xhprof::Hello();
---
+++ src/Xhprof/Sdk/Xhprof.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Xhprof\Sdk;
+
+class Xhprof
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function Hello()
+    {
+        echo 'Hello World';
+    }
+}
